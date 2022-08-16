f1f162386a95106af2b086404cb0de219f859936
Dell <Dell@DESKTOP-EGOPRNC>
2022-08-16 10:34:30 +0530

    Hello1

---
+++ src/gittest/Test2.java
@@ -0,0 +1,26 @@
+package gittest;
+
+public class Test2 {
+	
+	public static void main(String[]args) {
+	
+	
+	System.out.println("Test one is pass");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}}
