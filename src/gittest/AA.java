b2eaecadb0a78d7759927ead27e4a3c869dfd3cc
Ganeshital <Ganeshital@ShitalGanesh>
2022-08-17 10:20:50 +0530

    dfdsf

---
+++ src/gittest/AA.java
@@ -0,0 +1,25 @@
+package gittest;
+
+public class AA {
+	public AA()
+	{
+		this(47);
+		System.out.println("1st constructor");
+	}
+	public AA(int a)
+	{
+		this(45,"shubham");
+		System.out.println("2nd constructor");
+	}
+	public AA(int a,String x)
+	{
+		System.out.println("3nd constructor");
+	}
+	
+	
+	public static void main(String[] args) {
+		//AA S=new AA();
+		AA w=new AA(76);
+		
+	}
+}
